431a13cd615887f63d65d9b0abe0810dad372232
TechHack3 <140188790+TechHack3@users.noreply.github.com>
2023-09-23 15:44:50 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1,1 +1,12 @@
-
+contributors = [
+  {
+    id: 1,
+    fullname: "sample 1",
+    username: "https://github.com/TechHack3",
+  },
+   {
+    id: 2,
+    fullname: "sample 2",
+    username: "https://github.com/TechHack3",
+  }
+  ];
