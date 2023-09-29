f982ade18e6c5479a4ce8e47e1e49f59f05d399e
Mitesh2020 <miteshofficial007@gmail.com>
2023-09-29 14:29:33 +0530

    Updated ContributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -8,5 +8,10 @@ contributors = [
     id: 2,
     fullname: "sample 2",
     username: "https://github.com/TechHack3",
+  },
+  {
+    id: 3,
+    fullname: "Mitesh Rathod",
+    username: "https://github.com/Mitesh2020",
   }
   ];
