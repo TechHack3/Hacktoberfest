fb3f917d6c689ab646ffb8df3cac538fe7bad91b
Md. Habibur Rahman Shohel <md.hrshohel@gmail.com>
2023-10-01 02:04:27 +0600

    list update push 1

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -8,5 +8,10 @@ contributors = [
     id: 2,
     fullname: "sample 2",
     username: "https://github.com/TechHack3",
+  },
+  {
+    id: 3,
+    fullname: "sample 3",
+    username: "https://github.com/MdHRShohel"
   }
   ];
