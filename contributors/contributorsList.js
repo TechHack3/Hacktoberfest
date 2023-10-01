5e24c52fe66bf668c5dc50fd2479d58f717faaea
Bryan King Pecho <bkpecho05@gmail.com>
2023-10-01 12:05:29 +0800

    feat: Guess The Number Game

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -4,9 +4,14 @@ contributors = [
     fullname: "sample 1",
     username: "https://github.com/TechHack3",
   },
-   {
+  {
     id: 2,
     fullname: "sample 2",
     username: "https://github.com/TechHack3",
+  },
+  {
+    id: 3,
+    fullname: "Bryan King Pecho",
+    username: "https://github.com/bkpecho",
   }
-  ];
+];
