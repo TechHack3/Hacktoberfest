fa74351dd3a0b71edf8d2ffb715ac031163b601e
Vanshika Gupta <114987374+gva-cse@users.noreply.github.com>
2023-10-02 17:40:51 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -8,5 +8,10 @@ contributors = [
     id: 2,
     fullname: "sample 2",
     username: "https://github.com/TechHack3",
+  },
+   {
+    id: 3,
+    fullname: "Vanshika Gupta",
+    username: "https://github.com/gva-cse",
   }
   ];
