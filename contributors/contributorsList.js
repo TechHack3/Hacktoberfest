31e7f4a98856faba131df8b81c7a026b329aad41
debasisnishank <nishankdebasis4@gmail.com>
2023-10-04 15:30:27 +0530

    added project shows weather info

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -1,7 +1,12 @@
 contributors = [
-   {
+  {
     id: 1,
+    fullname: "sample 1",
+    username: "https://github.com/TechHack3",
+  },
+  {
+    id: 2,
     fullname: "Debasis Nishank",
     username: "https://github.com/debasisnishank",
   }
-  ];
+];
