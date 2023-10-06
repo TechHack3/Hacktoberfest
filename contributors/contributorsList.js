91b7c9b3e27f31d9b1f779ca9dc51558c38e6b8e
Suraj-Wakka <58338343+surajwakka@users.noreply.github.com>
2023-10-06 10:40:55 +0530

    Update contributorsList.js

--- contributors/contributorsList.js
+++ contributors/contributorsList.js
@@ -8,5 +8,10 @@ contributors = [
     id: 2,
     fullname: "sample 2",
     username: "https://github.com/TechHack3",
+  },
+  {
+    id: 3,
+    fullname: "Suraj Vishwakarma",
+    username: "https://github.com/surajwakka",
   }
   ];
